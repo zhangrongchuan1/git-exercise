8f55cde467b088559ced3c122fda111354483bf8
J <jonas.unterweger@web.de>
2025-10-22 17:04:34 +0200

    Added the body to the test function for vector subtraction

--- exercise1.py
+++ exercise1.py
@@ -46,4 +46,7 @@ def test_3d_vector_addition() -> None:
 
 def test_3d_vector_subtraction() -> None:
     # Task B: add a test for vector subtraction
-    assert False
+    v = Vector([1.1, 2.2, 3.3]) - Vector([1.0, 2.0, 3.0])
+    assert isclose(v[0], 0.1)
+    assert isclose(v[1], 0.2)
+    assert isclose(v[2], 0.3)
